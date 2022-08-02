55746f5b759d66bfa95446796863fb468c20ff2b
siraj <itssiraj@gmail.com>
2022-08-02 19:39:45 +0500

    Deploying EC2 in newly deployed VPC

--- examples/testing-logic/testing.tf
+++ examples/testing-logic/testing.tf
@@ -3,6 +3,11 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+data "aws_key_pair" "example" {
+  key_name           = "AlwaysEC2"
+  include_public_key = true
+}
+
 output "avalibility_zone" {
   value = length(data.aws_availability_zones.available.names)
 }
@@ -18,12 +23,44 @@ output "vpc_cidr" {
 output "public-subnets" {
     value = module.vpc.public-subnets[1].availability_zone
 }
+output "security-group" {
+  value = module.vpc.security_group[0]
+  
+}
+
+/*output "fingerprint" {
+  value = data.aws_key_pair.example
+}*/
+
+locals {
+  user_data = <<-EOT
+  #! /bin/bash
+  sudo apt-get update
+  sudo apt-get install -y apache2
+  sudo systemctl start apache2
+  sudo systemctl enable apache2
+  echo "The page was created by the user data" | sudo tee /var/www/html/index.html
+  EOT
+}
 
 module "vpc" {
     source = "../../modules/vpc"
     infra_env = "TerraformTest"
     vpc_cidr = "10.12.0.0/16"
-    vpc_public_subnet_cidr = ["10.12.1.0/24", "10.12.2.0/24","10.12.3.0/24", "10.12.4.0/24", "10.12.5.0/24", "10.12.6.0/24","10.12.7.0/24", "10.12.8.0/24"]
-    vpc_private_subnet_cidr = ["10.12.21.0/24", "10.12.20.0/24"]
+    vpc_public_subnet_cidr = ["10.12.1.0/24", "10.12.2.0/24" ]
+    #vpc_private_subnet_cidr = ["10.12.21.0/24", "10.12.22.0/24"]
+    vpc_private_subnet_cidr = [ ]
 
+}
+
+module "ec2" {
+    source = "../../modules/ec2"
+    infra_env = "Terraform"
+    subnets = keys(module.vpc.vpc_public_subnets)
+    key_name = "AlwaysEC2"
+    vpc_security_group_ids = [ module.vpc.security_group[0].id ]
+    user_data_base64 = base64encode(local.user_data)
+    user_data_replace_on_change = true
+    instance_type = "t2.micro"
+    instance_ami = "ami-0cff7528ff583bf9a"
 }
